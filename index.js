052519394b196dcd239c50bab3c0e1093dee3a72
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-10-11 21:38:48 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("Hello, world");
+var name = "Michael";
+var a = 2;
+var b = 2.5;
+var result = parseInt(a + b);
+var c;
+console.log(c);
+var b = null;
+console.log(b);
+
+var bool = true;
+var bool2 = false;
+
+console.log(bool); console.log(bool2);
+
+var d = 5;
+var g = 2;
+var s = d % g;
+console.log(s);
+console.log(d % g);
+// =, ==
+//conditional operators
+
+if (2 + 2 == 4) { // >, >=, <, <=, ==
+  console.log("HAPPY");
+}
+var car = "ZHIGULI";
+var beautifulCar = ""
+
+if (car == "Mers") {
+  console.log("EQUAL");
+} else if(car == "ZHIGULI") {
+  console.log("ZHIGULI");
+} else if (car == "Nissan") {
+  console.log("ETO NISSAN");
+} else {
+  console.log("NOT EQUAL");
+}
